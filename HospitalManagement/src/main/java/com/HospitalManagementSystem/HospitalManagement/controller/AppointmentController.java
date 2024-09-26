40da13e11392e9eafd56ccca17136341b7ed714c
surajchaudhari2002 <chaudharisuraj2002@gmail.com>
2024-09-26 12:34:07 +0530

    controller is designed

--- HospitalManagement/src/main/java/com/HospitalManagementSystem/HospitalManagement/controller/AppointmentController.java
+++ HospitalManagement/src/main/java/com/HospitalManagementSystem/HospitalManagement/controller/AppointmentController.java
@@ -1,4 +1,26 @@
 package com.HospitalManagementSystem.HospitalManagement.controller;
 
+import com.HospitalManagementSystem.HospitalManagement.entity.Appointment;
+import com.HospitalManagementSystem.HospitalManagement.repositery.AppointmentRepositery;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v2")
+
 public class AppointmentController {
+    @Autowired
+    AppointmentRepositery appointmentRepositery;
+
+    @PostMapping("/insert")
+    public Appointment createAppointment(@RequestBody Appointment appointment){
+        return appointmentRepositery.save(appointment);
+    }
+
+    @GetMapping
+    public List<Appointment> getAppointment(){
+        return appointmentRepositery.findAll();
+    }
 }
--- HospitalManagement/src/main/java/com/HospitalManagementSystem/HospitalManagement/controller/MedicineController.java
+++ HospitalManagement/src/main/java/com/HospitalManagementSystem/HospitalManagement/controller/MedicineController.java
@@ -1,4 +1,27 @@
 package com.HospitalManagementSystem.HospitalManagement.controller;
 
+import com.HospitalManagementSystem.HospitalManagement.entity.Medicine;
+import com.HospitalManagementSystem.HospitalManagement.repositery.MedicineRepositery;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v3")
+
 public class MedicineController {
+    @Autowired
+    MedicineRepositery medicineRepositery;
+
+    @PostMapping("/insert")
+    public Medicine createMedicine(@RequestBody Medicine medicine){
+        return medicineRepositery.save(medicine);
+    }
+    public List<Medicine> getMedicine(){
+        return medicineRepositery.findAll();
+    }
 }
