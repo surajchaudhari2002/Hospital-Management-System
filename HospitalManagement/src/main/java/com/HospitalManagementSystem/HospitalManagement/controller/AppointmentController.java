ac0a6593320d378a2df73cee2d7f04c8e5fbd4c1
surajchaudhari2002 <chaudharisuraj2002@gmail.com>
2024-10-02 17:01:23 +0530

    delete api is creted

--- HospitalManagement/src/main/java/com/HospitalManagementSystem/HospitalManagement/controller/AppointmentController.java
+++ HospitalManagement/src/main/java/com/HospitalManagementSystem/HospitalManagement/controller/AppointmentController.java
@@ -3,9 +3,13 @@ package com.HospitalManagementSystem.HospitalManagement.controller;
 import com.HospitalManagementSystem.HospitalManagement.entity.Appointment;
 import com.HospitalManagementSystem.HospitalManagement.repositery.AppointmentRepositery;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.management.AttributeNotFoundException;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @CrossOrigin(origins = "http://localhost:4200")
@@ -23,5 +27,15 @@ public class AppointmentController {
     @GetMapping("/appointment")
     public List<Appointment> getAppointment(){
         return appointmentRepositery.findAll();
+    }
+
+    @DeleteMapping("/appointment/{id}")
+    public ResponseEntity<Map<String,Boolean>> deleteAppointment(@PathVariable long id) throws AttributeNotFoundException {
+        Appointment appointment=appointmentRepositery.findById(id).orElseThrow(()->new AttributeNotFoundException("Appointment not found with id "+id));
+        appointmentRepositery.delete(appointment);
+        Map<String,Boolean> response=new HashMap<String,Boolean>();
+        response.put("Deleted",Boolean.TRUE);
+        return ResponseEntity.ok(response);
+
     }
 }
--- HospitalManagement/src/main/java/com/HospitalManagementSystem/HospitalManagement/controller/PatientController.java
+++ HospitalManagement/src/main/java/com/HospitalManagementSystem/HospitalManagement/controller/PatientController.java
@@ -1,12 +1,17 @@
 package com.HospitalManagementSystem.HospitalManagement.controller;
 
+import com.HospitalManagementSystem.HospitalManagement.entity.Appointment;
 import com.HospitalManagementSystem.HospitalManagement.entity.Patient;
 import com.HospitalManagementSystem.HospitalManagement.repositery.PatientRepositery;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
+import javax.management.AttributeNotFoundException;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @CrossOrigin(origins = "http://localhost:4200")
 @RestController
@@ -26,4 +31,13 @@ public class PatientController {
         return patientRepositery.findAll();
     }
 
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Map<String,Boolean>> deletePatient(@PathVariable long id) throws AttributeNotFoundException {
+        Patient patient=patientRepositery.findById(id).orElseThrow(()->new AttributeNotFoundException("Patient not found with id "+id));
+        patientRepositery.delete(patient);
+        Map<String,Boolean> response=new HashMap<String,Boolean>();
+        response.put("Deleted",Boolean.TRUE);
+        return ResponseEntity.ok(response);
+
+    }
 }
